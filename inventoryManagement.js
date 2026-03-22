bc57a1803c7fd06b5b21eb54520295cc00073a27
tuyishimejeanpierre-byte <tuyishime.jeanpierre@student.moringaschool.com>
2026-03-22 12:10:12 +0300

    update added content

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -3,10 +3,32 @@
 
 
 // Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+// module.exports = {
+//   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+//   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+//   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+//   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+//   products
+// };
+
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+function logFirstProduct(){
+  console.log(products[0]);
+
+}
+
+
+function updateProductName(index, newName){
+  products[index] = newName;
+   
+}
+
+
+function removeLastProduct() {
+  products.pop();
+}
+function addProduct(newProduct){
+  products.push(newProduct);
+}
+module.exports = {products, logFirstProduct, updateProductName,removeLastProduct, addProduct };
+
